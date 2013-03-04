dca03de4df5ea5ebb21ac7cdb8e967045bea921f
Tom Short <t.short@ieee.org>
2013-03-04 13:01:37 -0500

    Add working but commented out example using StrPack to set the Jacobian.

--- examples/cvode_Roberts_dns.jl
+++ examples/cvode_Roberts_dns.jl
@@ -23,10 +23,15 @@ function g(t, y, gout, user_data)
     return int32(0)
 end
 
-## Jacobian routine. Compute J(t,y) = df/dy.   BROKEN - JJ is wrong
+## Jacobian routine. Compute J(t,y) = df/dy.  
 
-## Attempt to use strpack
-## type J_DlsMat
+# Using StrPack
+# -- it works
+# -- it's clunky and dependent on Sundials version
+# Note: this is for Sundials v 2.4; the structure changed for v 2.5
+# Because of this, I'm commenting this out.
+## using StrPack
+## @struct type J_DlsMat    
 ##     typ::Int32
 ##     M::Int32
 ##     N::Int32
@@ -34,39 +39,40 @@ end
 ##     mu::Int32
 ##     ml::Int32
 ##     s_mu::Int32
-##     data::Vector{Float64}
+##     data::Ptr{Float64}
 ##     ldata::Int32
-##     cols::Array{Float64,2}
+##     cols::Ptr{Ptr{Float64}}
 ## end
-## io = IOBuffer()
-## dlsmat = pack(io, J_DlsMat(int32(0), int32(0), int32(0), int32(0), int32(0), int32(0), int32(0),
-##                   zeros(3), int32(0), zeros(3,3)))
-## Ndlsmat = length(io)
-## close(io)
-    
+# Note: Here is the (untested) structure for v 2.5
+## using StrPack
+## @struct type J_DlsMat    
+##     typ::Int32
+##     M::Int
+##     N::Int
+##     ldim::Int
+##     mu::Int
+##     ml::Int
+##     s_mu::Int
+##     data::Ptr{Float64}
+##     ldata::Int
+##     cols::Ptr{Ptr{Float64}}
+## end
 
+# The following works if the code above is uncommented.
 function Jac(N, t, y, fy, Jptr, user_data,
              tmp1, tmp2, tmp3)
-    println("jac0")
     y = Sundials.asarray(y)
-    ## global dlsmat = unpack(IOBuffer(pointer_to_array(convert(Ptr{Uint8}, Jptr), Ndlsmat)), J_DlsMat)
-    global dlsmat = unpack(IOBuffer(pointer_to_array(convert(Ptr{Uint8}, Jptr), (1000,))), J_DlsMat)
-    global J = dlsmat.cols
-    
-    ## unsafe_ref(convert(Ptr{Float64}, J + 40), (3,3))
-    ## global GJ = J
-    ## J = pointer_to_array(convert(Ptr{Float64}, J + 40), (3,3))  # the + 40 is funky pointer arithmetic
-    ## @show J
-    ## JJ = pointer_to_array(convert(Ptr{Float64}, GJ + 40), (3,3))  # the + 40 is funky pointer arithmetic
-    ## @show JJ
-    ## @show pointer_to_array(convert(Ptr{Int32}, GJ), (12,))  # the + 40 is funky pointer arithmetic
-    ## J[1,1] = -0.04
-    ## J[1,2] = 1.0e4*y[3]
-    ## J[1,3] = 1.0e4*y[2]
-    ## J[2,1] = 0.04 
-    ## J[2,2] = -1.0e4*y[3] - 6.0e7*y[2]
-    ## J[2,3] = -1.0e4*y[2]
-    ## J[3,2] = 6.0e7*y[2]
+    dlsmat = unpack(IOString(pointer_to_array(convert(Ptr{Uint8}, Jptr),
+                                              (sum(map(sizeof, J_DlsMat.types))+10,))),
+                    J_DlsMat)
+    J = pointer_to_array(unsafe_ref(dlsmat.cols), (int(neq), int(neq)), false)
+    J[1,1] = -0.04
+    J[1,2] = 1.0e4*y[3]
+    J[1,3] = 1.0e4*y[2]
+    J[2,1] = 0.04 
+    J[2,2] = -1.0e4*y[3] - 6.0e7*y[2]
+    J[2,3] = -1.0e4*y[2]
+    J[3,2] = 6.0e7*y[2]
     return int32(0)
 end
 
@@ -85,7 +91,7 @@ flag = Sundials.CVodeInit(cvode_mem, f, t0, y)
 flag = Sundials.CVodeSVtolerances(cvode_mem, reltol, abstol)
 flag = Sundials.CVodeRootInit(cvode_mem, int32(2), g)
 flag = Sundials.CVDense(cvode_mem, neq)
-## flag = Sundials.CVDlsSetDenseJacFn(cvode_mem, Jac)
+## flag = Sundials.CVDlsSetDenseJacFn(cvode_mem, Jac)  # works, but clunky, see above
 
 iout = 0
 tout = t1
