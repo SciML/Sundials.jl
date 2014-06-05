d9b42603e2b4c00f7f19bcbeaa2ee57286f58a6c
acroy <alexcroy@googlemail.com>
2014-06-05 19:55:02 +0200

    Add some tests.

---
+++ test/runtests.jl
@@ -0,0 +1,31 @@
+using Sundials
+using Base.Test
+
+# run cvode example
+println("== start cvode example")
+include("../examples/cvode_Roberts_simplified.jl")
+
+println("result at t=$(t[end]):")
+println(res[end,:], "\n")
+
+
+# run ida example
+println("== start ida example")
+include("../examples/ida_Roberts_simplified.jl")
+
+println("result at t=$(t[end]):")
+println(res[end,:], "\n")
+
+
+# run kinsol example
+println("== start kinsol example")
+include("../examples/kinsol_mkin_simplified.jl")
+
+println("solution:")
+println(res)
+residual = ones(2)
+sysfn(res, residual)
+println("residual:")
+println(residual, "\n")
+
+@test abs(minimum(residual)) < 1e-5
