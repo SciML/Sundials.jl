9d556395b65ed530f843dc313d61ff7e83bb67f6
Christian Haargaard Olsen <chaarga@ncsu.edu>
2015-05-26 21:10:48 +0200

    Cleaned up formatting in CVodes_Roberts_FSA_dns example

--- examples/cvodes_Roberts_FSA_dns.jl
+++ examples/cvodes_Roberts_FSA_dns.jl
@@ -1,19 +1,23 @@
 #######################################################################
 #       Implementation of Sundials forward sensitivity example        #
-#                              in Julia                               #
+#                  CVode_Roberts_FSA_dns in Julia.                    #
 #######################################################################
-
 
 using Sundials
 using StrPack
 
-##
+
+#######################################################################
+#              Problem related functions and structures               #
+#######################################################################
+
+# Create a user data structure that can be passed through to
+# the right hand side function
 type UserData
   p :: Array{Float64,1}
 end
 
 ## f routine. Compute function f(t,y). 
-
 function f(t, y, ydot, user_data)
   y = Sundials.asarray(y)
 
@@ -24,51 +28,34 @@ function f(t, y, ydot, user_data)
   p1 = user_data.p[1]
   p2 = user_data.p[2]
   p3 = user_data.p[3]
+
   ydot = Sundials.asarray(ydot)
+
   ydot[1] = -p1*y[1] + p2*y[2]*y[3]
   ydot[3] = p3*y[2]*y[2]
   ydot[2] = -ydot[1] - ydot[3]
+
   return int32(0)
   end
-
-function fnopar(t, y, ydot, user_data)
-  y = Sundials.asarray(y)
-  ydot = Sundials.asarray(ydot)
-  ydot[1] = -0.04*y[1] + 1.0e4*y[2]*y[3]
-  ydot[3] = 3.0e7*y[2]*y[2]
-  ydot[2] = -ydot[1] - ydot[3]
-  return int32(0)
-  end
-
-
-
-## g routine. Compute functions g_i(t,y) for i = 0,1. 
-
-function g(t, y, gout, user_data)
-  y = Sundials.asarray(y)
-  gout = Sundials.asarray(gout, (2,))
-  gout[1] = y[1] - 0.0001
-  gout[2] = y[3] - 0.01
-  return int32(0)
-  end
-
 
 # Note: Here is the (untested) structure for v 2.5
+# This is taken from cvode_Roberts_dns.jl and have been given no additional thoughts.
 using StrPack
 @struct type J_DlsMat    
-  typ::Int32
-  M::Int
-  N::Int
-  ldim::Int
-  mu::Int
-  ml::Int
-  s_mu::Int
-  data::Ptr{Float64}
-  ldata::Int
-  cols::Ptr{Ptr{Float64}}
+  typ   :: Int32
+  M     :: Int
+  N     :: Int
+  ldim  :: Int
+  mu    :: Int
+  ml    :: Int
+  s_mu  :: Int
+  data  :: Ptr{Float64}
+  ldata :: Int
+  cols  :: Ptr{Ptr{Float64}}
   end
 
-## Jacobian routine. Compute J(t,y) = df/dy.  
+# Jacobian routine. Compute J(t,y) = df/dy.  
+# Again - taken from cvode_Roberts_dns.jl. But with parameter values being parsed instead of hardcoded
 function Jac(N, t, y, fy, Jptr, user_data,
              tmp1, tmp2, tmp3)
   y = Sundials.asarray(y)
@@ -81,6 +68,7 @@ function Jac(N, t, y, fy, Jptr, user_data,
   p1 = user_data.p[1]
   p2 = user_data.p[2]
   p3 = user_data.p[3]
+
   J[1,1] = -p1
   J[1,2] = p2*y[3]
   J[1,3] = p2*y[2]
@@ -91,7 +79,7 @@ function Jac(N, t, y, fy, Jptr, user_data,
   return int32(0)
   end
 
-# Function for derivatives
+#= fS routine. Compute sensitivity r.h.s.  =#
 function fS(Ns, t, y, ydot, 
               iS, yS, ySdot, 
               user_data, tmp, tmp2) 
@@ -118,18 +106,15 @@ function fS(Ns, t, y, ydot,
   sd2 = -sd1-sd3;
 
   if iS == 0
-    #println("is = 1")
     sd1 += -y1;
     sd2 +=  y1;
   elseif iS == 1
-    #println("is = 2")
     sd1 +=  y2*y3;
     sd2 += -y2*y3;
   elseif iS == 2
-    #println("is = 3")
     sd2 += -y2*y2;
     sd3 +=  y2*y2;
-  end
+    end
   
   ySdot[1] = sd1
   ySdot[2] = sd2
@@ -142,6 +127,7 @@ function fS(Ns, t, y, ydot,
 function ewt(y, w, user_data; reltol = 1e-4, abstol = [1e-8, 1e-14, 1e-6])
   y = Sundials.asarray(y)
   w = Sundials.asarray(w)
+
   user_data = unsafe_pointer_to_objref(user_data) :: UserData
 
   for i=1:3
@@ -157,17 +143,21 @@ function ewt(y, w, user_data; reltol = 1e-4, abstol = [1e-8, 1e-14, 1e-6])
   end
 
 
+#######################################################################
+#                     Functions for pretty output                     #
+#######################################################################
+
 # Print output pretty like the C program
 function PrintOutput(cvode_mem, t, u)
   udata = Sundials.asarray(u)
 
-  nst = [int64(0)]
-  qu = [int32(0)]
-  hu = [0.0]
-  
-  flag = Sundials.CVodeGetNumSteps(cvode_mem,pointer(nst))
-  flag = Sundials.CVodeGetLastOrder(cvode_mem,qu)
-  flag = Sundials.CVodeGetLastStep(cvode_mem,pointer(hu))
+  nst   = [int64(0)]
+  qu    = [int32(0)]
+  hu    = [0.0]
+
+  flag  = Sundials.CVodeGetNumSteps(cvode_mem,pointer(nst))
+  flag  = Sundials.CVodeGetLastOrder(cvode_mem,qu)
+  flag  = Sundials.CVodeGetLastStep(cvode_mem,pointer(hu))
 
   @printf("%8.3e %2d  %8.3e %5d \n", t[1], qu[1], hu[1], nst[1]);
   @printf("                  Solution       ");
@@ -197,23 +187,23 @@ function PrintOutputS(uS)
 
 # Print output pretty like the C program
 function PrintFinalStats(cvode_mem, sensi)
-  nst = [int64(0)]
-  nfe = [int64(0)]
+  nst      = [int64(0)]
+  nfe      = [int64(0)]
 
-  nsetups = [int64(0)]
-  nni = [int64(0)]
-  ncfn = [int64(0)]
-  netf = [int64(0)]
+  nsetups  = [int64(0)]
+  nni      = [int64(0)]
+  ncfn     = [int64(0)]
+  netf     = [int64(0)]
 
-  nfSe = [int64(0)]
-  nfeS = [int64(0)]
+  nfSe     = [int64(0)]
+  nfeS     = [int64(0)]
   nsetupsS = [int64(0)]
-  nniS = [int64(0)]
-  ncfnS = [int64(0)]
-  netfS = [int64(0)]
+  nniS     = [int64(0)]
+  ncfnS    = [int64(0)]
+  netfS    = [int64(0)]
 
-  nje = [int64(0)]
-  nfeLS = [int64(0)]
+  nje      = [int64(0)]
+  nfeLS    = [int64(0)]
 
 
   flag = Sundials.CVodeGetNumSteps(cvode_mem, pointer(nst));
@@ -253,6 +243,11 @@ function PrintFinalStats(cvode_mem, sensi)
 
   end
 
+
+#######################################################################
+#                        Start of main program                        #
+#######################################################################
+
 # Calculate sensitivities?
 sensi      = true
 sensi_meth = int32(Sundials.CV_SIMULTANEOUS) # CV_SIMULTANEOUS, CV_STAGGERED or CV_STAGGERED1
@@ -279,16 +274,17 @@ cvode_mem = Sundials.CVodeCreate(Sundials.CV_BDF, Sundials.CV_NEWTON)
 flag = Sundials.CVodeInit(cvode_mem, f, t0, y)
 
 #= Use private function to compute error weights =#
-flag = Sundials.CVodeWFtolerances(cvode_mem, ewt);
+flag = Sundials.CVodeWFtolerances(cvode_mem, ewt)
 
 #= Attach user data =#
-flag = Sundials.CVodeSetUserData(cvode_mem, data);
+flag = Sundials.CVodeSetUserData(cvode_mem, data)
 
 #= Attach linear solver =#
 flag = Sundials.CVDense(cvode_mem, neq)
 
 #= Set Jacobian =#
-flag = Sundials.CVDlsSetDenseJacFn(cvode_mem, Jac)  # works, but clunky, see above and cvode_Roberts_dns.jl
+# works, but clunky, see above and cvode_Roberts_dns.jl
+flag = Sundials.CVDlsSetDenseJacFn(cvode_mem, Jac)  
 
 
 #= Sensitivity related settings =#
@@ -296,7 +292,7 @@ if sensi
   pbar = data.p
 
   # yS should be an array of arrays
-  ySJ = Array(Array{Float64,1}, neq)
+  ySJ    = Array(Array{Float64,1}, neq)
   ySJ[1] = zeros(Float64,NS)
   ySJ[2] = zeros(Float64,NS)
   ySJ[3] = zeros(Float64,NS)
@@ -309,41 +305,37 @@ if sensi
   ## ySJpointer is then equal to ySjulia
 
   flag = Sundials.CVodeSensInit1(cvode_mem, NS, sensi_meth, fS, yS)
-  flag = Sundials.CVodeSensEEtolerances(cvode_mem);
-  flag = Sundials.CVodeSetSensErrCon(cvode_mem, err_con);
-  flag = Sundials.CVodeSetSensParams(cvode_mem, 0, Sundials.nvector(data.p), 0);
+  flag = Sundials.CVodeSensEEtolerances(cvode_mem)
+  flag = Sundials.CVodeSetSensErrCon(cvode_mem, err_con)
+  flag = Sundials.CVodeSetSensParams(cvode_mem, 0, Sundials.nvector(data.p), 0)
   end
 
 iout = 1
 tout = t1
 t = [t0]
 
-@printf("\n\n");
-@printf("===========================================");
-@printf("============================\n");
-@printf("     T     Q       H      NST           y1");
-@printf("           y2           y3    \n");
-@printf("===========================================");
-@printf("============================\n");
+@printf("\n\n")
+@printf("===========================================")
+@printf("============================\n")
+@printf("     T     Q       H      NST           y1")
+@printf("           y2           y3    \n")
+@printf("===========================================")
+@printf("============================\n")
 
 while iout<=nout
   flag = Sundials.CVode(cvode_mem, tout, y, t, Sundials.CV_NORMAL)
   PrintOutput(cvode_mem, t, y)
   if sensi
-    flag = Sundials.CVodeGetSens(cvode_mem, t, yS);
+    flag   = Sundials.CVodeGetSens(cvode_mem, t, yS)
     tempyS = [unsafe_load(yS,1), unsafe_load(yS,2), unsafe_load(yS,3)]
     
     PrintOutputS(yS)
-    #println("Sens1: ", Sundials.asarray(tempyS[1]))
-    #println("Sens2: ", Sundials.asarray(tempyS[2]))
-    #println("Sens3: ", Sundials.asarray(tempyS[3]))
-    #println()
   end
-  @printf("-----------------------------------------");
-  @printf("------------------------------\n");
+  @printf("-----------------------------------------")
+  @printf("------------------------------\n")
 
     iout += 1
-    tout *= tmult;
+    tout *= tmult
   end
 
 PrintFinalStats(cvode_mem, sensi);
