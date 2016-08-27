00e59799886895f4daa5231e89fad6cefaffc1c5
Alexey Stukalov <astukalov@gmail.com>
2016-08-27 23:08:31 +0200

    runtests: run each example in separate let block

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,42 +5,51 @@ examples_path = joinpath(dirname(@__FILE__), "..", "examples")
 
 # run cvode example
 println("== start cvode Roberts example (simplified)")
+let
 include(joinpath(examples_path, "cvode_Roberts_simplified.jl"))
-
 println("result at t=$(t[end]):")
 println(res[end,:], "\n")
+end
 
 println("== start cvode Roberts example")
+let
 include(joinpath(examples_path, "cvode_Roberts_dns.jl"))
+end
 
 # run ida examples
 println("== start ida_Roberts example (simplified)")
+let
 include(joinpath(examples_path, "ida_Roberts_simplified.jl"))
+end
 
 println("== start ida_Roberts example")
+let
 include(joinpath(examples_path, "ida_Roberts_dns.jl"))
-
 println("result at t=$(t[end]):")
 println(yout[end,:], "\n")
+end
 
 println("== start ida_Heat2D example")
+let
 include(joinpath(examples_path, "ida_Heat2D.jl"))
-
 println("result at t=$(t[end]):")
 println(yout[end,:], "\n")
+end
 
 # run kinsol example
 println("== start kinsol example (simplified)")
+let
 include(joinpath(examples_path, "kinsol_mkin_simplified.jl"))
-
 println("solution:")
 println(res)
 residual = ones(2)
 sysfn(res, residual)
 println("residual:")
 println(residual, "\n")
+end
 
 println("== start kinsol example")
+let
 include(joinpath(examples_path, "kinsol_mkinTest.jl"))
-
 @test abs(minimum(residual)) < 1e-5
+end
