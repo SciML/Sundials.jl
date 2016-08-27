d77b7ae43a41e447672f6ab918d8264105de1524
Alexey Stukalov <astukalov@gmail.com>
2016-08-27 23:05:56 +0200

    runtests: more robust examples_path

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,37 @@
 using Sundials
 using Base.Test
 
+examples_path = joinpath(dirname(@__FILE__), "..", "examples")
+
 # run cvode example
 println("== start cvode Roberts example (simplified)")
-include("../examples/cvode_Roberts_simplified.jl")
+include(joinpath(examples_path, "cvode_Roberts_simplified.jl"))
 
 println("result at t=$(t[end]):")
 println(res[end,:], "\n")
 
 println("== start cvode Roberts example")
-include("../examples/cvode_Roberts_dns.jl")
+include(joinpath(examples_path, "cvode_Roberts_dns.jl"))
 
 # run ida examples
 println("== start ida_Roberts example (simplified)")
-include("../examples/ida_Roberts_simplified.jl")
+include(joinpath(examples_path, "ida_Roberts_simplified.jl"))
 
 println("== start ida_Roberts example")
-include("../examples/ida_Roberts_dns.jl")
+include(joinpath(examples_path, "ida_Roberts_dns.jl"))
 
 println("result at t=$(t[end]):")
 println(yout[end,:], "\n")
 
 println("== start ida_Heat2D example")
-include("../examples/ida_Heat2D.jl")
+include(joinpath(examples_path, "ida_Heat2D.jl"))
 
 println("result at t=$(t[end]):")
 println(yout[end,:], "\n")
 
 # run kinsol example
 println("== start kinsol example (simplified)")
-include("../examples/kinsol_mkin_simplified.jl")
+include(joinpath(examples_path, "kinsol_mkin_simplified.jl"))
 
 println("solution:")
 println(res)
@@ -39,6 +41,6 @@ println("residual:")
 println(residual, "\n")
 
 println("== start kinsol example")
-include("../examples/kinsol_mkinTest.jl")
+include(joinpath(examples_path, "kinsol_mkinTest.jl"))
 
 @test abs(minimum(residual)) < 1e-5
