4842e3835d5a64f8c30dc5830bb0c15bf51e513e
ChrisRackauckas <me@chrisrackauckas.com>
2017-03-31 09:03:02 -0700

    no printing

--- test/handle_tests.jl
+++ test/handle_tests.jl
@@ -1,4 +1,3 @@
-println("Test empty isempty")
 h1 = Sundials.Handle(Sundials.CVodeCreate(Sundials.CV_BDF, Sundials.CV_NEWTON))
 h2 = h1
 
@@ -10,6 +9,5 @@ empty!(h1)
 @test isempty(h1)
 @test isempty(h2)
 
-println("Handle pointing to NULL")
 h = Sundials.Handle(h1.ptr_ref[])
 @test isempty(h)
