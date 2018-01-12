3414dd993b516d3bfcbe629be644f2aa240f8bd7
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-11 16:49:04 -0800

    mem fix

--- src/handle.jl
+++ src/handle.jl
@@ -39,6 +39,7 @@ immutable Handle{T <: AbstractSundialsObject}
     end
 end
 
+Base.unsafe_convert{T}(::Type{Ptr{Void}}, h::Handle{T}) = h.ptr_ref[]
 Base.convert{T}(::Type{Ptr{T}}, h::Handle{T}) = h.ptr_ref[]
 Base.convert{T}(::Type{Ptr{Ptr{T}}}, h::Handle{T}) = convert(Ptr{Ptr{T}}, h.ptr_ref[])
 
