dd1987d69f7b64221d670723afe2393e58268708
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-05-10 06:30:15 -0700

    Update callbacks.jl

--- src/common_interface/callbacks.jl
+++ src/common_interface/callbacks.jl
@@ -136,7 +136,8 @@ function apply_callback!(integrator,callback::ContinuousCallback,cb_time,prev_si
   end
   saved_in_cb = false
 
-  @inbounds if callback.save_positions[1]
+  # Pre-callback save, but don't double up if unnecessary
+  @inbounds if callback.save_positions[1] && integrator.t != sol.t[end]
     savevalues!(integrator,true)
     saved_in_cb = true
   end
@@ -172,7 +173,8 @@ end
 @inline function apply_discrete_callback!(integrator,callback::DiscreteCallback)
   saved_in_cb = false
   if callback.condition(integrator.u,integrator.t,integrator)
-    @inbounds if callback.save_positions[1]
+    # Pre-callback save, but don't double up if unnecessary
+    @inbounds if callback.save_positions[1] && integrator.t != sol.t[end]
       savevalues!(integrator,true)
       saved_in_cb = true
     end
