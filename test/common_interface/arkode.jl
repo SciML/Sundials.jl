2cec5a22dcf5d500c8962692f7497a59a3ba7806
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-13 06:47:01 -0700

    SplitFunction

--- test/common_interface/arkode.jl
+++ test/common_interface/arkode.jl
@@ -8,10 +8,9 @@ sol = solve(prob,ARKODE())
 f1 = (du,u,p,t) -> du .= u
 f2 = (du,u,p,t) -> du .= u
 
-prob = SplitODEProblem(f1,f2,rand(4,2),(0.0,1.0))
-function (::typeof(prob.f))(::Type{Val{:analytic}},u0,p,t)
-    exp(2t)*u0
-end
+f = SplitFunction(f1,f2,analytic=(u0,p,t)->exp(2t)*u0)
+prob = SplitODEProblem(f,rand(4,2),(0.0,1.0))
+
 sol = solve(prob,ARKODE())
 @test sol.errors[:l2] < 1e-2
 sol = solve(prob,ARKODE(),reltol=1e-12,abstol=1e-12)
