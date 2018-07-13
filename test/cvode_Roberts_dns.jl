aa6bf72f13e99f65ceaefcbf342f90b30fc46088
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-12 23:14:19 -0700

    fix cfunctions

--- test/cvode_Roberts_dns.jl
+++ test/cvode_Roberts_dns.jl
@@ -1,4 +1,4 @@
-using Sundials, Compat
+using Sundials
 
 ## f routine. Compute function f(t,y).
 
@@ -55,9 +55,9 @@ cvode_mem = Sundials.Handle(mem_ptr)
 userfun = Sundials.UserFunctionAndData(f, userdata)
 Sundials.CVodeSetUserData(cvode_mem, userfun)
 
-Sundials.CVodeInit(cvode_mem, Sundials.local_cfunction(Sundials.cvodefun,
-                    Cint, (Sundials.realtype, Sundials.N_Vector,
-                    Sundials.N_Vector, Ref{typeof(userfun)})), t1,
+Sundials.CVodeInit(cvode_mem, Sundials.old_cfunction(Sundials.cvodefun,
+                    Cint, Tuple{Sundials.realtype, Sundials.N_Vector,
+                    Sundials.N_Vector, Ref{typeof(userfun)}}), t1,
                     convert(Sundials.N_Vector, y0))
 Sundials.@checkflag Sundials.CVodeInit(cvode_mem, f, t0, y0)
 Sundials.@checkflag Sundials.CVodeSVtolerances(cvode_mem, reltol, abstol)
