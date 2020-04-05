2c30311676502483036c95b52402c5d81b61c5b6
Yingbo Ma <mayingbo5@gmail.com>
2020-04-04 21:08:05 -0400

    Update ARKODE tests

--- test/common_interface/arkode.jl
+++ test/common_interface/arkode.jl
@@ -5,21 +5,21 @@ import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinea
 
 prob = prob_ode_linear
 dt = 1//2^(4)
-sol = solve(prob,ARKStep())
+sol = solve(prob,ARKODE())
 @test sol.errors[:l2] < 5e-2
-sol = solve(prob,ARKStep(),abstol=[1e-7])
+sol = solve(prob,ARKODE(),abstol=[1e-7])
 
 f1 = (du,u,p,t) -> du .= u
 f2 = (du,u,p,t) -> du .= u
 
 prob = prob_ode_2Dlinear
 dt = 1//2^(4)
-sol = solve(prob,ARKStep())
+sol = solve(prob,ARKODE())
 
 prob = SplitODEProblem(SplitFunction(f1,f2,analytic=(u0,p,t)->exp(2t)*u0),
                        rand(4,2),(0.0,1.0))
 
-sol = solve(prob,ARKStep())
+sol = solve(prob,ARKODE())
 @test sol.errors[:l2] < 1e-2
-sol = solve(prob,ARKStep(),reltol=1e-12,abstol=1e-12)
+sol = solve(prob,ARKODE(),reltol=1e-12,abstol=1e-12)
 @test sol.errors[:l2] < 1e-6
