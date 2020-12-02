36e23b181eef1a2f6aec983db5d675366d9ab8d5
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-12-01 20:06:06 -0500

    Update errors.jl

--- test/common_interface/errors.jl
+++ test/common_interface/errors.jl
@@ -8,7 +8,7 @@ prob = ODEProblem(f_error, u0, (0.0, 1.0))
 sol = solve(prob, CVODE_BDF())
 sol = solve(prob, CVODE_BDF(), verbose = false)
 
-f_error2(du, u, p, t) = u / t - 1
+f_error2(du, u, p, t) = du .= u ./ t .- 1
 u0 = [1.0];
 du0 = [1.0];
 prob = DAEProblem(f_error2, u0, du0, (0.0, 1.0), differential_vars = [1])
