cb25e315ebe36e4997ce0fdda0fe881da0c2e48b
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-14 07:34:27 -0400

    Fix retcode reinit
    Fixes https://github.com/SciML/DifferentialEquations.jl/issues/861 for Sundials

--- test/common_interface/iterators.jl
+++ test/common_interface/iterators.jl
@@ -24,3 +24,21 @@ solve!(integrator)
 u2 = copy(integrator.u)
 @test u1 != vec(prob.u0)
 @test u1 == u2
+
+f(u, p, t) = 2 * u
+u_positive(u, t, integrator) = u > 0
+terminate_if_u_pos = DiscreteCallback(u_positive, terminate!)
+
+prob = ODEProblem(f, [1.0], (0.0, 1.0)) # positive initial condition > positive u > :Terminated
+integrator = init(prob, CVODE_BDF(), callback=terminate_if_u_pos)
+sol1 = solve!(integrator)
+@test sol1.retcode === :Terminated
+
+reinit!(integrator, -1.0) # negative initial condition > negative u > :Success!
+sol2 = solve!(integrator)
+@test sol2.retcode == :Success
+
+prob = ODEProblem(f, -1.0, (0.0, 1.0)) # negative initial condition > negative u > :Success!
+integrator = init(prob, Tsit5(), callback=terminate_if_u_pos)
+sol3 = solve!(integrator)
+@test sol3.retcode == :Success
