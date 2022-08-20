0386577d3f2c4da1a813ed41e219d29945918552
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-20 09:15:17 -0400

    change to ODEProblemLibrary

--- test/common_interface/iterators.jl
+++ test/common_interface/iterators.jl
@@ -1,7 +1,7 @@
 using DiffEqProblemLibrary, Sundials, Test
 using DiffEqProblemLibrary.ODEProblemLibrary: importodeproblems;
 importodeproblems();
-import DiffEqProblemLibrary.ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear
+import ODEProblemLibrary: prob_ode_linear, prob_ode_2Dlinear
 
 prob = prob_ode_2Dlinear
 integrator = init(prob, CVODE_BDF())
