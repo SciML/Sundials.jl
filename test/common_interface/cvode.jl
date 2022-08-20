4e2a4c4c386d8744e61aad8ff41a5ee57625dc79
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-20 13:20:35 -0400

    no error calcuation

--- test/common_interface/cvode.jl
+++ test/common_interface/cvode.jl
@@ -54,7 +54,8 @@ sol_idxs = solve(prob, CVODE_Adams(); save_idxs = [1], timeseries_errors = false
 
 @test sol[1, :] == sol_idxs[1, :]
 
-sol_idxs = solve(prob, CVODE_Adams(); save_idxs = [1, 2], timeseries_errors = false)
+sol_idxs = solve(prob, CVODE_Adams(); save_idxs = [1, 2], timeseries_errors = false,
+                 calculate_error = false)
 @test length(sol_idxs[1]) == 2
 @test sol[1, :] == sol_idxs[1, :]
 @test sol[2, :] == sol_idxs[2, :]
