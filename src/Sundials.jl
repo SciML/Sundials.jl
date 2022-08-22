821b62146579c7bcfe8f9676329e593070d87490
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-22 05:09:20 -0400

    add precompile snooping

--- src/Sundials.jl
+++ src/Sundials.jl
@@ -83,6 +83,52 @@ include("common_interface/integrator_types.jl")
 include("common_interface/integrator_utils.jl")
 include("common_interface/solve.jl")
 
+import SnoopPrecompile
+
+SnoopPrecompile.@precompile_all_calls begin
+    function lorenz(du, u, p, t)
+        du[1] = 10.0(u[2] - u[1])
+        du[2] = u[1] * (28.0 - u[3]) - u[2]
+        du[3] = u[1] * u[2] - (8 / 3) * u[3]
+    end
+
+    function lorenz_oop(u, p, t)
+        [10.0(u[2] - u[1]), u[1] * (28.0 - u[3]) - u[2], u[1] * u[2] - (8 / 3) * u[3]]
+    end
+
+    solver_list = [
+        ARKODE(), CVODE_Adams(), CVODE_BDF(),
+    ]
+
+    prob_list = [ODEProblem(lorenz, [1.0; 0.0; 0.0], (0.0, 1.0));
+                 ODEProblem{true, false}(lorenz, [1.0; 0.0; 0.0], (0.0, 1.0));
+                 ODEProblem{true, false}(lorenz, [1.0; 0.0; 0.0], (0.0, 1.0), Float64[]);
+                 ODEProblem(lorenz_oop, [1.0; 0.0; 0.0], (0.0, 1.0));
+                 ODEProblem{false, false}(lorenz_oop, [1.0; 0.0; 0.0], (0.0, 1.0));
+                 ODEProblem{false, false}(lorenz_oop, [1.0; 0.0; 0.0], (0.0, 1.0),
+                                          Float64[])]
+
+    for prob in prob_list, solver in solver_list
+        solve(prob, solver)(5.0)
+    end
+
+    prob_list = nothing
+
+    function f(out, du, u, p, t)
+        out[1] = -0.04u[1] + 1e4 * u[2] * u[3] - du[1]
+        out[2] = +0.04u[1] - 3e7 * u[2]^2 - 1e4 * u[2] * u[3] - du[2]
+        out[3] = u[1] + u[2] + u[3] - 1.0
+    end
+    u₀ = [1.0, 0, 0]
+    du₀ = [-0.04, 0.04, 0.0]
+    tspan = (0.0, 100000.0)
+    differential_vars = [true, true, false]
+    prob = DAEProblem(f, du₀, u₀, tspan, differential_vars = differential_vars)
+    sol = solve(prob, IDA())
+
+    prob = nothing
+end
+
 ##################################################################
 # Deprecations
 ##################################################################
