45d4b8044b360b102b2203949c188f8fa5423761
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-08-22 17:35:56 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,2 @@
 using Sundials
-using Test
-
-# Some tests use @cfunction, but at the moment that isn't supported on non-Intel platforms
-const SUPPORT_CFUNCTION = Sys.ARCH ∉ (:aarch64, :ppc64le, :powerpc64le) ||
-                          startswith(lowercase(String(Sys.ARCH)), "arm")
-@testset "Generator" begin include("generator.jl") end
-
 @testset "Preconditioners" begin include("common_interface/precs.jl") end
