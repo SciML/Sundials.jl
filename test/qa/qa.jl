58b9b1442188c243b589947d92067881455eb1ff
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-11 08:01:25 -0400

    Use shared rendered API docs QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,8 +1,6 @@
 using SciMLTesting, Sundials, Test
 using JET
 
-include("public_api_docs.jl")
-
 # ExplicitImports ignore-lists below are all names owned by / non-public in OTHER
 # packages that Sundials legitimately uses; they will stop being flagged once those
 # upstream packages mark them public.
